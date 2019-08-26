844f7d896c02f2206e9de890a4b3539f318aa4dc
啊旺 <986870079@qq.com>
2019-08-26 17:20:47 +0800

    还原简单

--- js/scramble.js
+++ js/scramble.js
@@ -14,8 +14,8 @@ function scrambledString(tag, objName, initScrambledString, initScrambledStringI
     this.bubbleSortBookmark = 0;
 
     this.rescramble();
-    //this.tag.innerHTML = this.string + '<br>[ <a href="#" onClick="' + this.objName + '.initAnimatedBubbleSort();return false;">reveal</a> ]';
-    this.tag.innerHTML = this.string + '<br>[ <a href="#" onClick="' + this.objName + '.initAnimatedBubbleSort();insertpic();return false;">reveal</a> ]';//加了个图片。。但这样把代码改得很乱，没有模块化。
+    this.tag.innerHTML = this.string + '<br>[ <a href="#" onClick="' + this.objName + '.initAnimatedBubbleSort();return false;">reveal</a> ]';
+    //this.tag.innerHTML = this.string + '<br>[ <a href="#" onClick="' + this.objName + '.initAnimatedBubbleSort();insertpic();return false;">reveal</a> ]';//加了个图片。。但这样把代码改得很乱，没有模块化。
 }
 
 function rescramble() {
